2fce2f49eac6e65267c50ce52491cc1e64edfedb
freshsponge <brendanilers@gmail.com>
2022-08-28 10:36:57 -0500

    Added Drag code

--- TapStore/AppsViewController.swift
+++ TapStore/AppsViewController.swift
@@ -341,27 +341,125 @@ private extension AppsViewController {
      
     }
 }
-//
-//extension AppsViewController: UIDragInteractionDelegate, UIDropInteractionDelegate {
-//
-//}
 
-//extension AppsViewController {
-//  func dragItems(for indexPath: IndexPath) -> [UIDragItem] {
-//    let dragSection = sections[indexPath.item]
-//    let itemProvider = NSItemProvider(object: dragSection.name as NSString)
-//    let dragItem = UIDragItem(itemProvider: itemProvider)
-//    return [dragItem]
-//  }
-//}
-//
-//extension AppsViewController: UICollectionViewDragDelegate {
-//  func collectionView(_ collectionView: UICollectionView,
-//                      itemsForBeginning session: UIDragSession,
-//                      at indexPath: IndexPath) -> [UIDragItem] {
-//    let dataSource = dataSourceForCollectionView(collectionView)
-//    return dataSource.dragItems(for: indexPath)
-//  }
-//}
+extension AppsViewController: UICollectionViewDelegate { }
 
+
+extension AppsViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//        return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: supplementaryViewIdentifier, for: indexPath)
+//    }
+}
+
+
+extension AppsViewController: UICollectionViewDragDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let itemProvider = NSItemProvider(object: "\(indexPath)" as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = sections[indexPath.section]
+        return [dragItem]
+    }
+
+    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem] {
+        let itemProvider = NSItemProvider(object: "\(indexPath)" as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = data[indexPath.section][indexPath.row]
+        return [dragItem]
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, dragSessionWillBegin session: UIDragSession) {
+        var itemsToInsert = [IndexPath]()
+        (0 ..< data.count).forEach {
+            itemsToInsert.append(IndexPath(item: data[$0].count, section: $0))
+            data[$0].append(.availableToDropAtEnd)
+        }
+        collectionView.insertItems(at: itemsToInsert)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, dragSessionDidEnd session: UIDragSession) {
+        var removeItems = [IndexPath]()
+        for section in 0..<data.count {
+            for item in  0..<data[section].count {
+                switch data[section][item] {
+                    case .availableToDropAtEnd:
+                        removeItems.append(IndexPath(item: item, section: section))
+                    case .simple:
+                        break
+                }
+            }
+        }
+        removeItems.forEach { data[$0.section].remove(at: $0.item) }
+        collectionView.deleteItems(at: removeItems)
+    }
+    
+    
+}
+
+
+
+
+extension SecondController: UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        let destinationIndexPath: IndexPath
+        if let indexPath = coordinator.destinationIndexPath {
+            destinationIndexPath = indexPath
+        } else {
+            // useless, just in case
+            let section = collectionView.numberOfSections - 1
+            let row = collectionView.numberOfItems(inSection: section)
+            destinationIndexPath = IndexPath(row: row, section: section)
+        }
+
+        switch coordinator.proposal.operation {
+            case .move:
+                reorderItems(coordinator: coordinator, destinationIndexPath:destinationIndexPath, collectionView: collectionView)
+            default:
+                break
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool {
+        return true
+    }
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+        // made the above logic useless
+        if collectionView.hasActiveDrag, destinationIndexPath != nil {
+            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        else {
+            return UICollectionViewDropProposal(operation: .forbidden)
+        }
+    }
+
+    
+    
+    private
+    func reorderItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
+        let items = coordinator.items
+        if items.count == 1, let item = items.first,
+            let sourceIndexPath = item.sourceIndexPath,
+            let localObject = item.dragItem.localObject as? CellModel {
+
+            collectionView.performBatchUpdates ({
+                data[sourceIndexPath.section].remove(at: sourceIndexPath.item)
+                data[destinationIndexPath.section].insert(localObject, at: destinationIndexPath.item)
+                collectionView.deleteItems(at: [sourceIndexPath])
+                collectionView.insertItems(at: [destinationIndexPath])
+            })
+        }
+    }
+
+   
+}
 
